d427e5777ec943941156907fdb3a69040dc15151
zp4rker <acc@zp4rker.com>
2021-03-02 22:56:16 +1100

    :heart_eyes:

--- src/main/kotlin/discore/panel/PanelHost.kt
+++ src/main/kotlin/discore/panel/PanelHost.kt
@@ -1,57 +1,65 @@
 package discore.panel
 
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.routing.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import org.json.JSONArray
+import org.json.JSONObject
 import java.net.ServerSocket
 import java.net.Socket
+import java.util.*
 import java.util.concurrent.Executors
 import kotlin.concurrent.thread
-import kotlin.system.exitProcess
 
 /**
  * @author zp4rker
  */
-val connections = mutableListOf<SocketRunnable>()
+val connections = mutableListOf<SocketConnection>()
 
 fun main() {
+    embeddedServer(Netty, port = 8080) {
+        routing {
+            get ("/list-channels/{id}") {
+                val guild = call.request.queryParameters["guild"]
+                val name = call.request.queryParameters["name"]
+                val request = JSONObject()
+                request.put("command", "list channels")
+                val options = JSONObject()
+                if (guild != null) options.put("guild", guild)
+                if (name != null) options.put("name", name)
+                request.put("options", options)
+
+                connections.find { it.id.toString() == call.parameters["id"] }?.apply {
+                    wr.write("$request\n")
+                    wr.flush()
+
+                    val response = JSONObject(rd.readLine())
+                    call.respondText(response.toString(2))
+                }
+            }
+        }
+    }.start()
+
     val ss = ServerSocket(6000)
     val exec = Executors.newCachedThreadPool()
 
     thread {
-        run@ while (true) {
+        while (true) {
             val connection = ss.accept()
-            exec.submit(SocketRunnable(connection))
+            connections.add(SocketConnection(connection))
         }
     }
 
     println("Listening to connections on: ${ss.inetAddress.hostName}:${ss.localPort}")
 }
 
-class SocketRunnable(private val connection: Socket) : Runnable {
-    private val rd = connection.getInputStream().bufferedReader()
-    private val wr = connection.getOutputStream().bufferedWriter()
+class SocketConnection(connection: Socket, val id: UUID = UUID.randomUUID()) {
+    val rd = connection.getInputStream().bufferedReader()
+    val wr = connection.getOutputStream().bufferedWriter()
 
     init {
-        connections.add(this)
-    }
-
-    override fun run() {
-        while (true) {
-            var line = rd.readLine()
-            while (line != null) {
-                if (line != "quit") {
-                    connections.filter { it != this }.forEach { it.sendMessage("[${connection.let { c -> "${c.inetAddress.hostName}:${c.port}" }}]: $line") }
-                    sendMessage("[You]: $line")
-                    println("Received message from ${connection.let { "${it.inetAddress.hostName}:${it.port}" }}")
-                    line = rd.readLine()
-                } else {
-                    println("Closing server...")
-                    exitProcess(0)
-                }
-            }
-        }
-    }
-
-    fun sendMessage(message: String) {
-        wr.write("$message\n")
-        wr.flush()
+        println("$id has connected!")
     }
 }
